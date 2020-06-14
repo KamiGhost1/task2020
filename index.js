03053348e223a051a328a12a84f859b52ad2179c
Roman <romires2000@gmail.com>
2020-06-14 23:36:40 +0300

    setting project

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-let express = require('express');
+const express = require('express');
+const fs = require('fs');
+const md5 = require('md5');
+const request = require('request');
+const bP = require('body-parser');
+const cP = require('cookie-parser');
 
 
-let port = 3000;
+const port = 3000;
+const dir = __dirname
 
 let app = express();
+
+app.use(cP())
+app.use(bP.urlencoded({extended:false}))
+app.use(bP.json())
 app.use((req,res,next)=>{
     console.log(chalk.green(getTime())+' '+chalk.blue(req.method)+' '+chalk.red(req.url))
     next()
 })
 
+app.route('/')
+    .get((req,res)=>{
 
+    })
 
 app.listen(port, (err) => {
     if (err) {
