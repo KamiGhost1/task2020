271d0632e455ecb5ab7b9a16de0bc8c7c1d4fa92
Roman <romires2000@gmail.com>
2020-06-15 19:17:07 +0300

    add scripts

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,35 @@ let dropCookie = function(){
     document.cookie = 'id = ;max-age=-1'
 }
 
+let getTask = function () {
+    $.get('/task/get',viewTask)
+}
+
+let viewTasks = function (data,status) {
+    let table = document.getElementById('content');
+    let html = '';
+    data.forEach(el=>{
+        html+=`<tr><td onclick="changeName(${el.id})">${el.name}</td><td>${el.task}</td><td onclick="changeStatus(${el.id})">${statusRender(el.status)}</td><td><button class="btn btn-success" onclick="viewTask(${el.id})">Смотреть</button></td></tr>`;
+    })
+    table.innerHTML = html;
+}
+
+let changeName = function(id){
+
+}
+
+let statusRender = function(status){
+
+}
+
+let changeStatus = function(id){
+
+}
+
+let viewTask = function (id) {
+
+}
+
 function getUrlVars()
 {
     return window.location.href.slice(window.location.href.indexOf('?')).split(/[&?]{1}[\w\d]+=/);
